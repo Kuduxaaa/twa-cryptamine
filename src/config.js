2a7c68779c20ab2d8dba9ea3806d8a743352f72c
Kuduxaaa <nikakuduxashvili0@gmail.com>
2024-06-10 01:25:23 +0400

    some changes

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const Config = {
+    baseUrl: 'https://kuduxaaa.github.io/twa-cryptamine'
+}
+
+export default Config;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import Config from "./config";
+
 // this manifest is used temporarily for development purposes
-const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+const manifestUrl = `${Config.baseUrl}/manifest.json`;
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
