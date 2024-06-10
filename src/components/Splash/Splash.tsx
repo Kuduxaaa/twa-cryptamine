53f7566d3cacc6903e74e4b382a0c0275c25ac75
Kuduxaaa <nikakuduxashvili0@gmail.com>
2024-06-10 06:19:22 +0400

    fixed-logo

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -22,7 +22,7 @@ const Splash: React.FC<SplashProps> = ({ onLoaded }) => {
         return (<div className='splash'>
             <span className="loader"></span>
             <div className='loader-wrapper'>
-                <img src="/logo-circle.png" draggable="false" alt="Cryptamine" className='logo-image' />
+                <img src="logo-circle.png" draggable="false" alt="Cryptamine" className='logo-image' />
                 <p className='title'>Cryptamine</p>
                 <p>Please wait...</p>
             </div>
